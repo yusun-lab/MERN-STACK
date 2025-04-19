d276717f8041260bc5b5713023e01a82a7395263
yusun-lab <yu.sun@hyperisland.se>
2025-04-19 11:50:46 +0200

    setup backend server with Express, dotenv, and nodemon, test with REST client and  Postman

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+require('dotenv').config(); // Load environment variables from .env file
+
+const express = require('express');
+
+// express app
+const app = express();
+
+//global middleware
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next()
+}) 
+
+// routes
+app.get('/', (req, res) => {
+  console.log('🔥 GET / route triggered');
+  res.json({message: 'Welcome to the app!'});
+})
+
+// listen for requests
+app.listen(process.env.PORT, () => {
+  console.log('listening on port', process.env.PORT);
+})
