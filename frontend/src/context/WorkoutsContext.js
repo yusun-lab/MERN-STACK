10d84a3a412084ddd0835771018016bd289ce6f2
yusun-lab <yu.sun@hyperisland.se>
2025-05-01 19:06:23 +0200

    Handle delete functionality

--- frontend/src/context/WorkoutsContext.js
+++ frontend/src/context/WorkoutsContext.js
@@ -5,12 +5,16 @@ export const WorkoutsContext = createContext();
 export const workoutsReducer = (state, action) => {
   switch (action.type) {
     case 'SET_WORKOUTS': 
-    return {
-      workouts: action.payload
-    }
+      return {
+        workouts: action.payload
+      }
     case 'CREATE_WORKOUT':
       return {
         workouts: [action.payload, ...state.workouts]
+      }
+    case 'DELETE_WORKOUT':
+      return {
+        workouts: state.workouts.filter((w) => w._id !== action.payload._id)
       }
     default:
       return state
